4f120fccdd62ef2ddbc5a12c24d3ace3f44f98d1
liangshuo <liangshuoone@aliyun.com>
2022-08-01 09:37:34 +0800

    feat: add typescript-doc link

--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>About TypeScript</h2>
+    <h3>
+      <a href="https://www.tslang.cn/" target="_blank" rel="noopener">typescript doc</a>
+    </h3>
   </div>
 </template>
 
