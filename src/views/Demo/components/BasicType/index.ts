48f59e105d058d85a3c61edb771c69ff6dbb253c
liangshuo <liangshuoone@aliyun.com>
2022-08-12 17:21:51 +0800

    feat: 增加格式化配置

--- src/views/Demo/components/BasicType/index.ts
+++ src/views/Demo/components/BasicType/index.ts
@@ -2,7 +2,7 @@
  * @Author: liangshuo liangshuoone@aliyun.com
  * @Date: 2022-08-12 16:52:57
  * @LastEditors: liangshuo liangshuoone@aliyun.com
- * @LastEditTime: 2022-08-12 16:55:29
+ * @LastEditTime: 2022-08-12 17:11:08
  */
 import BasicType from './BasicType.vue';
 
